bf909a626e3e41ae66a65551313047a935d4f0e0
Debopri-yo <ghoshlipika53@gmail.com>
2025-09-04 21:32:21 +0530

    add a form and button for adding new book

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ const myLibrary=[];
 function addBookToLibrary(title,author,pages,read){
   const book = new Book(title,author,pages,read);
   myLibrary.push(book);
+  renderBooks();
 }
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
-const container=document.querySelector('#container');
-myLibrary.forEach(book => {
+function renderBooks() {
+  const container=document.querySelector('#container');
+  container.innerhtml="";
+  myLibrary.forEach(book => {
   const bookCard = document.createElement("div")
   bookCard.className = "Books";
   
@@ -27,5 +30,29 @@ myLibrary.forEach(book => {
   bookCard.appendChild(list)
   
   container.appendChild(bookCard);
-})
+  })
+}
+//dialogs and stuff
+const dialogElem = document.getElementById("dialog");
+const showBtn = document.querySelector(".show");
+const closeBtn = document.querySelector(".close");
 
+showBtn.addEventListener("click", () => {
+  dialogElem.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  dialogElem.close();
+});
+//form submission
+const form = document.getElementById("form");
+form.addEventListener("submit",(event)=>{
+  event.preventDefault();
+  const title=document.getElementById("title").value;
+  const author=document.getElementById("author").value;
+  const pages=document.getElementById("pages").value;
+  const read=document.getElementById("read").checked;
+  addBookToLibrary(title,author,pages,read);
+  form.reset();
+  dialogElem.close();
+});
