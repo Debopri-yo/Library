feefc609119b3b32eb4d2fb51e165412966cdc72
Debopri-yo <ghoshlipika53@gmail.com>
2025-09-04 19:28:37 +0530

    display books info on page

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ function Book(title,author,pages,read){
   this.author=author;
   this.pages=pages;
   this.read=read;
-  this.info=function(){
-    return this.title+" by "+this.author+", "+this.pages+" pages, "+(this.read ? "already read" : "not read yet");
-  };
+  this.id=crypto.randomUUID();
 }
+Book.prototype.info=function(){
+    return this.title+" by "+this.author+", "+this.pages+" pages, "+(this.read ? "already read" : "not read yet");
+}
+const myLibrary=[];
+function addBookToLibrary(title,author,pages,read){
+  const book = new Book(title,author,pages,read);
+  myLibrary.push(book);
+}
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+const container=document.querySelector('#container');
+myLibrary.forEach(book => {
+  const bookCard = document.createElement("div")
+  bookCard.className = "Books";
+  
+  const list = document.createElement("p")
+  const text=(book.info());
+  list.innerText = `${text}`
+  bookCard.appendChild(list)
+  
+  container.appendChild(bookCard);
+})
 
